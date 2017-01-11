334eec66bba612e58d35cd64a7c2da8e84d64fd5
Stephen Hung <hung.stephen.yh@gmail.com>
2017-01-10 17:50:56 -0800

    Finished exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,13 +4,27 @@
 # the sentence of the form
 #   "The difference in length is N"
 
+CompareLength <- function(str1,str2){
+  length <- abs(nchar(str2) - nchar(str1))
+  return( paste("The difference in length is ",length) )
+}
 
 # Pass two strings of different lengths to your `CompareLength` function
 
+CompareLength("Hello World","Suh")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
 
+DescribeDifference <- function(str1,str2){
+  if(nchar(str1) > nchar(str2)){
+    return( paste("Your first string is longer by ",nchar(str1)-nchar(str2)) )
+  }else{
+    return( paste("Your second string is longer by ",nchar(str2)-nchar(str1)) )
+  }
+}
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+
+DescribeDifference("Stephen Hung","a string")
